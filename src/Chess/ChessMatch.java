b526dfce61200ad2ddf2f54bfd790a03bb62bdb6
henrique_rs <henriquers@alu.ufc.br>
2022-07-23 16:26:02 -0300

    Criando as peças Rei e Torre

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,12 +1,16 @@
 package Chess;
 
+import Chess.pieces.King;
+import Chess.pieces.Rook;
 import boargame.Board;
+import boargame.Position;
 
 public class ChessMatch {
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8,8);//o tabuleiro de xadrez possui oito linhas e oito colunas
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -17,5 +21,10 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE),new Position(2,4));
+		board.placePiece(new King(board,Color.BLACK), new Position(6,2));
 	}
 }
---
+++ src/Chess/pieces/King.java
@@ -0,0 +1,18 @@
+package Chess.pieces;
+
+import Chess.ChessPiece;
+import Chess.Color;
+import boargame.Board;
+
+public class King extends ChessPiece{
+
+	public King(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "K";
+	}
+
+}

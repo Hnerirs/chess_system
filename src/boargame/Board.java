53c45a9ebd54c73f02482d7e682669c92ce02d00
henrique_rs <henriquers@alu.ufc.br>
2022-07-23 16:58:23 -0300

    BoardException
    Exceções do TABULEIRO E proteção contra posições não existentes ou já ocupada

--- src/boargame/Board.java
+++ src/boargame/Board.java
@@ -5,7 +5,10 @@ public class Board {
 	private int columns;
 	private Piece[][] pieces;
 	public Board(int rows, int columns) {
-	
+		
+		if(columns<1 || rows<1) {
+			throw new BoardException("Numero de linhas ou colunas inválido: porfavor digite um numero entre 1 e 8");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -24,13 +27,40 @@ public class Board {
 	}
 	
 	public Piece piece(int row, int columns) {
+		if(!positionExists(row, columns)) {
+			throw new BoardException("Posição não encontrada no tabuleiro");
+		}
 		return pieces[row][columns];
 	}
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição não encontrada no tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	public void placePiece(Piece piece, Position position) {
+		
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Ha uma peça nessa posição");
+		}
+		
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row>=0 && row<this.rows && column >= 0 && column < this.columns;
+	}
+	
+	public boolean positionExists(Position posi) {
+		return positionExists(posi.getRow(),posi.getColumn());
+		
+	}
+	
+	public boolean thereIsAPiece(Position posi) {
+		if(!positionExists(posi)) {
+			throw new BoardException("Posição não existe no tabuleiro");
+		}
+		return piece(posi) != null;
 	}
 }
